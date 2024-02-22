9194ebf06e869d53c26a1c7264e8c6f5a0c7a861
anastasiadergaeva <adergaeva@gmail.com>
2024-02-22 21:26:54 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import './App.css';
 import TariffItem from './components/TariffItem.jsx';
-import data from './data.json';
 
 function App() {
-  const cardItem = data.map(item =>
-    <TariffItem
-      name={item.name}
-      price={item.price}
-      megabit={item.megabit}
-      trafficvolume={item.trafficvolume}
-    />
-  )
   return (
-    <main className='main'>{cardItem}</main>
+    <TariffItem />
   );
 }
 
--- src/components/TariffItem.jsx
+++ src/components/TariffItem.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import './tariff.scss'
-
+import './tariff.scss';
+import data from '../data.json';
+console.log(data);
 class TariffItem extends React.Component {
     render() {
-        const { name, price, megabit, trafficvolume } = this.props;
-        return (
+        const card = data.map(item =>
             <div className='card'>
-                <div className='card__header'>
-                    <h1>{name}</h1>
+                <div className={`card__header ${item.header}`}>
+                    <h1>{item.name}</h1>
                 </div>
-                <div className='card__price'>
-                    <p>руб {price}/месяц</p>
+                <div className={`card__price ${item.info}`}>
+                    <p className='currency'>руб</p>
+                    <p className='price'><span className='cost'>{item.price}</span>/месяц</p>
                 </div>
                 <div className='card__megabit'>
-                    <p>До {megabit} Мбит/сек</p>
+                    <p>До {item.megabit} Мбит/сек</p>
                 </div>
-                <div className='card__traffic'>{trafficvolume}</div>
+                <div className='card__traffic'>{item.trafficvolume}</div>
             </div >
+        )
+        return (
+            <main className='main'>{card}</main>
         )
     }
 }
