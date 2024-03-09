955dde8a8f9d4fdc4e348e54c83dca7a13ac60da
anastasiadergaeva <adergaeva@gmail.com>
2024-03-10 00:23:58 +0300

    add select

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
 import TariffItem from './components/TariffItem.jsx';
+import data from './data.json';
 
 function App() {
+  function getColor(index) {
+    switch (index) {
+      case 0:
+        return 'blue';
+      case 1:
+        return 'green';
+      case 2:
+        return 'red';
+      case 3:
+        return 'grey';
+      default:
+        return 'purple';
+    }
+  }
   return (
-    <TariffItem />
+    <main className='main'>
+      {
+        data.map((item, index) => {
+          return <TariffItem key={item.id} {...item} color={getColor(index)} />;
+        })
+      }
+    </main>
   );
 }
 
--- src/components/TariffItem.jsx
+++ src/components/TariffItem.jsx
@@ -1,28 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './tariff.scss';
-import data from '../data.json';
-console.log(data);
-class TariffItem extends React.Component {
-    render() {
-        const card = data.map(item =>
-            <div className='card'>
-                <div className={`card__header ${item.header}`}>
-                    <h1>{item.name}</h1>
-                </div>
-                <div className={`card__price ${item.info}`}>
-                    <p className='currency'>руб</p>
-                    <p className='price'><span className='cost'>{item.price}</span>/месяц</p>
-                </div>
-                <div className='card__megabit'>
-                    <p>До {item.megabit} Мбит/сек</p>
-                </div>
-                <div className='card__traffic'>{item.trafficvolume}</div>
-            </div >
-        )
-        return (
-            <main className='main'>{card}</main>
-        )
-    }
-}
 
-export default TariffItem;
+export default function TariffItem({ name, price, megabit, trafficvolume, color }) {
+    const [select, setSelect] = useState(false);
+    function handleClick() {
+        setSelect(!select);
+    }
+    return (
+        <div className={select ? `card ${color} selected` : `card ${color}`} onClick={handleClick}>
+            <div className="card__header">
+                <h1>{name}</h1>
+            </div>
+            <div className="card__price">
+                <p className="currency">руб</p>
+                <p className="price"><span className='cost'>{price}</span></p>
+            </div>
+            <div className="card__megabit">
+                <p>До {megabit} Мбит/сек</p>
+            </div>
+            <div className="card__traffic">{trafficvolume}</div>
+        </div>
+    )
+}
